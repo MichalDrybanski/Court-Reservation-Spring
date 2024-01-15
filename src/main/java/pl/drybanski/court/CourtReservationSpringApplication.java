28a9e9a7b861661f9dc6d2776cbd73fc1353071e
MichalD <michaeld13@interia.eu>
2024-01-15 16:18:44 +0100

    Initial commit

---
+++ src/main/java/pl/drybanski/court/CourtReservationSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.drybanski.court;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtReservationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtReservationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/drybanski/court/CourtReservationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.drybanski.court;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtReservationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
