eb091e4ff9845ad4de777860c81025f9722a568f
MichalD <michaeld13@interia.eu>
2024-01-29 18:33:33 +0100

    Plan

--- src/main/java/pl/drybanski/court/CourtReservationSpringApplication.java
+++ src/main/java/pl/drybanski/court/CourtReservationSpringApplication.java
@@ -11,3 +11,20 @@ public class CourtReservationSpringApplication {
 	}
 
 }
+//aplikacja posiada:
+
+//Kazdy moze utwozyc uzytkownika - wraz z nim tworzy się jego awatar - gracz
+//Kazdy gracz moze utwozyc grupe -> zostanie organizatorem
+//Kazdy organizator moze zaprosic do swojej grupy graczy
+//Kazdy organizator moze zorganizowac glosowanie w ramach grupy
+//Kazdy gracz w grupie moze oddać głos w głosowaniu
+//Organizator moze zamknąć(rozstrzygnąć) głosowanie
+//Organizator moze dopisać płatności do głosujących
+
+
+//Uzytkownicy(User)
+//Gracz(Player)
+//Grupa(Group)
+//Glosowanie(Voting)
+//Glos(Vote)
+//Platnosc(Payment)
